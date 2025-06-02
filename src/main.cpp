7c506e937f00376d3dc3b8d53b54a0ecf3d9a833
Tomas Kotulan <tkotulan@gmail.com>
2025-06-02 15:04:40 +0200

    Format code for consistency and readability in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,18 @@
 #include "SmartServoBus.hpp"
 #include "RBCX.h"
-#include<Arduino.h>
-#include<thread>
-auto &man = rb::Manager::get(); //pro fungovani RBCX
+#include <Arduino.h>
+#include <thread>
+auto &man = rb::Manager::get(); // pro fungovani RBCX
 #include "Grabber.hpp"
-#include"Comunication.hpp"
+#include "Comunication.hpp"
 #include "Movement.hpp"
-
 
 void setup()
 {
- 
-
+    Serial.begin(115200);
+    while (!Serial)
+    {
+        delay(10);
+    } // wait for serial port to connect. Needed for native USB
 }
-void loop(){}
+void loop() {}
